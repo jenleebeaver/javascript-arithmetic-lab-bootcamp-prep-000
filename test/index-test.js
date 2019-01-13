af494503e7071a1500b3986ff3f5ff2acbc27343
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-13 19:07:32 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,23 +1,30 @@
 var a, b
 
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
 var number;
 function add(a,b) {
   number = a + b;
   return number;
 }
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
 
+function subtract(a,b){
+  number = a - b;
+  return number;
+}
+
 it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
 })
+
+
 
 it('multiply(a, b) multiplies two numbers and returns the result', function() {
   expect(multiply(a, b)).toEqual(a * b)
