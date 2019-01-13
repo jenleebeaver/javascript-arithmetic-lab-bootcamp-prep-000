92075c78461c070225b1683eae105cc0e01d4278
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-13 19:19:42 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -80,6 +80,13 @@ describe('makeInt(n)', function() {
   })
 })
 
+function preserveDecimal(a) {
+  number = parseFloat('2.222', 10);
+  return number;
+}
+
+
+
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
